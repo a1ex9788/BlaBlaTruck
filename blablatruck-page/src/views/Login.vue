0de7dc678ef9081dcfe92a0dacb620aa67353307
jesgarsas <jesgarsas@gmail.com>
2020-10-16 11:37:12 +0200

    Añadido Axios y dinamismo a login

--- blablatruck-page/src/views/Login.vue
+++ blablatruck-page/src/views/Login.vue
@@ -5,9 +5,9 @@
         <h2 class="mt-3">Inicio de sesión</h2>
         <h5 class="mt-2">La nueva app de transportes eficientes!</h5>
 
-        <b-input class="mt-3" type="text" placeholder="Nombre de usuario"/>
-        <b-input class="mt-2" type="password" placeholder="Contraseña"/>
-        <button type="button" class="btn btn-primary mt-2 mb-2 btn-block" >Iniciar sesión</button>
+        <b-input class="mt-3" @input="isUsernameEmpty = undefined" :state="isUsernameEmpty" type="text" placeholder="Nombre de usuario" id="usernameText" trim/>
+        <b-input class="mt-2" @input="isPasswordEmpty = undefined" :state="isPasswordEmpty" type="password" placeholder="Contraseña" id="passwordText" trim/>
+        <button id="buttonLogin" @click="login" type="button" class="btn btn-primary mt-2 mb-2 btn-block">Iniciar sesión</button>
         <!--<a id="links">¿Ha olvidado su contraseña?</a>-->
         <div id="linksBottom" class="fixed-bottom mb-3">
             <a id="links">¿Quiere registrase como transportista?</a>
@@ -18,9 +18,36 @@
 </template>
 
 <script>
-
+/* eslint-disable */
 export default {
-    name: "Login"
+    name: "Login",
+    
+    data() {
+        return {
+            isUsernameEmpty: undefined,
+            isPasswordEmpty: undefined
+        };
+    },
+    methods: {
+        login () {
+            // Campo para saber si hay error
+            var errorLogin = false
+            //Recoger los datos de los campos
+            var usernameText = document.getElementById("usernameText")
+            var passwordText = document.getElementById("passwordText")
+            //Comprobación de campos con api
+            if (usernameText.value == "") { this.isUsernameEmpty = false; errorLogin = true}
+            if (passwordText.value == "") { this.isPasswordEmpty = false; errorLogin = true}
+            //Conexion a la api mediante axios
+
+            //Carga la interfaz del tipo de usuario si el login es satisfactorio 
+            //(de momento avisar si se ha hecho login bien)
+            console.log(usernameText.value)
+            if (!errorLogin) alert("Logged!")
+            else alert("Not Logged!") 
+        }
+    }
+
 }
 </script>
 
