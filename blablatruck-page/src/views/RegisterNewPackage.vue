0818786b82980daea328f1f3a99157653463b196
Octavia Daniela B <ocbu@outlook.es>
2020-10-18 20:46:12 +0200

    Modificada la interfaz para la creación de un pedido. Su rol tambien ha cambiado para el calculo del presupuesto segun los campos ha rellenar

--- blablatruck-page/src/views/RegisterNewPackage.vue
+++ blablatruck-page/src/views/RegisterNewPackage.vue
@@ -1,86 +1,125 @@
 <template>
-  <div id="groupForm" class="container-fluid">
-    <h1 class="mt-0">Crea tu encargo</h1>
+<div id="groupForm" class="container-fluid">
 
-    <form>
-        <label for="Nature" class="mt-3">Naturaleza</label>
-            <div class="col-mb">
-                <select id="Nature" class="form-control col-30">
-                    <option selected>Elige una opcion...</option>
-                    <option>Producto frágil</option>
-                    <option>Producto perecedero</option>
-                    <option>Mercancia peligrosa</option>
-                </select>
-            </div>  
-        
-        <label for="maxWeight" class="mt-3">Peso mercancia (en kg)</label>
-            <div class="col-mb">
-                <!--Añadir una restriccion de maxima carga 32-->
-                <input class="form-control" type="number" value="10" id="maxWeight">
-            </div>  
+    <div hidden id="showPriceCalculated">
+        <h1> Precio </h1>
+        <b-button class="mt-2 mx-4" type="submit" variant="primary">Crear encargo</b-button>
+        <b-button class="mt-2" type="button">Volver</b-button>
+    </div>
 
-        <form>
-            <label for="origin" class="mt-3">Tamaño</label> 
-            <div class="form-row">
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="Ancho">
-                </div>
-                <label class="mt-1">x</label>
-                <div class="col">
-                        <input type="text" class="form-control" placeholder="Alto">
-                </div>
-                <label class="mt-1">x</label>
-                <div class="col">
-                        <input type="text" class="form-control" placeholder="Largo">
-                </div>
-            </div>
-        </form>
+    <div id="priceCalculation">
 
-        <label for="origin" class="mt-3">Origen de envio</label>
-            <div class="col-mb">
-                <input class="form-control" type="text" id="origin" placeholder="Direccion: Calle | nrº | planta | puerta">
-            </div>
-       
-        <label for="destiny" class="mt-3">Destino de envio</label>
-            <div class="col-mb">
-                <input class="form-control" type="text"  id="destiny" placeholder="Direccion: Calle | nrº | planta | puerta">
-            </div>
-        
-        <div class="input-group mt-5">
-        <div class="input-group-prepend">
-            <button type="button" class="btn btn-outline-secondary">Calcular precio</button>   
-        </div>
-        <input type="text" class="form-control" placeholder="El precio calculado">
-        <span class="input-group-text" id="currency">€</span>
-        </div>
+        <h1 class="mt-0">Calcula el presupuesto de tu pedido</h1>
 
-        <button type="submit" class="btn btn-primary btn-lg mt-4 mx-3">Crear</button>
-        <button type="submit" class="btn btn-secondary btn-lg mt-4 mx-3">Volver</button>
-    </form>    
-    
-  </div>
+        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
 
+            <b-form-group id="input-group-1" label="Origen:" label-for="input-1">
+                <b-form-input id="input-Origin" type="text" required placeholder="Direccion: Calle | nrº | planta | puerta"></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-2" label="Destino:" label-for="input-2">
+                <b-form-input id="input-Destination" type="text" required placeholder="Direccion: Calle | nrº | planta | puerta"></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-3" label="Peso mercancia (en kg):" label-for="input-3">
+                <b-input-group class="mt-3 ">
+                    <template v-slot:append>
+                        <b-input-group-text><strong class="text">kg</strong></b-input-group-text>
+                    </template>
+                    <b-form-input id="input-Weight" max="32" min="0" value="1" type="number"></b-form-input>
+                </b-input-group>
+
+            </b-form-group>
+
+            <b-form-group id="input-group-Nature" label="Naturaleza:" label-for="input-Nature">
+                <b-form-select id="input-Nature" v-model="form.nature" :options="nature" required></b-form-select>
+            </b-form-group>
+
+            <label>Tamaño</label>
+
+            <b-form inline class="mt-2">
+
+                <b-input-group class="w-25">
+                    <template v-slot:append>
+                        <b-input-group-text><strong class="text">m</strong></b-input-group-text>
+                    </template>
+                    <b-form-input placeholder="Ancho"></b-form-input>
+                </b-input-group>
+
+                <label class="mt-2 mx-3">x</label>
+
+                <b-input-group class="w-25">
+                    <template v-slot:append>
+                        <b-input-group-text><strong class="text">m</strong></b-input-group-text>
+                    </template>
+                    <b-form-input placeholder="Alto"></b-form-input>
+                </b-input-group>
+
+                <label class="mt-2 mx-3">x</label>
+
+                <b-input-group class="w-25">
+                    <template v-slot:append>
+                        <b-input-group-text><strong class="text">m</strong></b-input-group-text>
+                    </template>
+                    <b-form-input placeholder="Largo"></b-form-input>
+                </b-input-group>
+
+            </b-form>
+
+            <br>
+            <b-button @click="showPrice" class="mt-2 mx-4" type="submit" variant="primary">Calcular</b-button>
+            <b-button class="mt-2" type="button">Volver</b-button>
+        </b-form>
+
+    </div>
+</div>
 </template>
 
 <script>
 export default {
-    name: "RegisterNewPackage",
-
-    data(){
-
-       
-
+    data() {
+        return {
+            form: {
+                origin: '',
+                destination: '',
+                weight: ''
+            },
+            nature: [{
+                text: 'Elige una opcion..',
+                value: null
+            }, 'Normal', 'Fragil', 'Congelado'],
+            show: true
+        }
     },
     methods: {
-        
+        onSubmit(evt) {
+            evt.preventDefault()
+            alert(JSON.stringify(this.form))
+        },
+
+        showPrice() {
+            document.getElementById("priceCalculation").hidden = true
+            document.getElementById("showPriceCalculated").hidden = false
+        },
+        onReset(evt) {
+            evt.preventDefault()
+            // Reset our form values
+            this.form.origin = ''
+            this.form.destination = ''
+            this.form.nature = null
+            // Trick to reset/clear native browser form validation state
+            this.show = false
+            this.$nextTick(() => {
+                this.show = true
+            })
+        }
     }
 }
 </script>
 
-
 <style>
- #groupForm {
-        max-width: 500px;
-        margin: 0 auto;
-    }
+#groupForm {
+    max-width: 500px;
+    margin: 0 auto;
+}
 </style>
