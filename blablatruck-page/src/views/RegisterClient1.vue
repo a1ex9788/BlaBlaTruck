65524094b868f9c882edd81f106ff6a250a31b40
jesgarsas <jesgarsas@gmail.com>
2020-10-18 19:29:40 +0200

    Añadido backend a RegisterClient2

--- blablatruck-page/src/views/RegisterClient1.vue
+++ blablatruck-page/src/views/RegisterClient1.vue
@@ -8,30 +8,65 @@
         <h3 id="subtitle" class="mt-3">Información personal</h3>
 
         <label id="labels" class="mt-3 mb-0"> Nombre </label>
-        <b-form-input type="text"> </b-form-input>
+        <b-form-input id="nombreText" type="text"> </b-form-input>
         <label id="labels" class="mt-3 mb-0"> Apellidos </label>
-        <b-form-input type="text"> </b-form-input>
+        <b-form-input id="apellidosText" type="text"> </b-form-input>
         <label id="labels" class="mt-3 mb-0"> Nombre de Usuario </label>
-        <b-form-input type="text"> </b-form-input>
-        <label id="labels" class="mt-3 mb-0"> Contraseña </label>
-        <b-form-input type="password"> </b-form-input>
+        <b-form-input id="usernameText" type="text"> </b-form-input>
+        <label id="labels"  class="mt-3 mb-0"> Contraseña </label>
+        <b-form-input id="contraseñaText" type="password"> </b-form-input>
         <label id="labels" class="mt-3 mb-0"> DNI </label>
-        <b-form-input type="text"> </b-form-input>
+        <b-form-input id="dniText" type="text"> </b-form-input>
         <label id="labels" class="mt-3 mb-0"> Empresa </label>
-        <b-form-input type="text"> </b-form-input>
+        <b-form-input id="empresaText" type="text"> </b-form-input>
         <label id="labels" class="mt-3 mb-0"> Teléfono </label>
-        <b-form-input type="tel"> </b-form-input>
+        <b-form-input id="telefonoText" type="tel"> </b-form-input>
+        <label id="labels" class="mt-3 mb-0"> Email </label>
+        <b-form-input id="emailText" type="tel"> </b-form-input>
 
         <label id="errorText" class="mt-4 mb-0"> Error </label>
 
         <router-link to="/login">
             <button id="buttons" class="btn btn-primary mb-2">Cancelar</button>
         </router-link>
-        <router-link to="/registerClient2">
-            <button id="buttons" class="btn btn-primary mb-2">Siguiente</button>
+        <router-link :to="{name:'RegisterClient2', params:{ nombre:this.nombre, apellidos:this.apellidos,
+             username:this.username, contraseña:this.contraseña, dni:this.dni, empresa:this.empresa, telefono:this.telefono, email:this.email}}">
+            <button id="buttons" @click="onClickSiguiente" class="btn btn-primary mb-2">Siguiente</button>
         </router-link>
     </div>
 </template>
+
+<script>
+
+export default {
+    data() {
+        return{
+            nombre: undefined,
+            apellidos: undefined,
+            username: undefined,
+            contraseña: undefined,
+            dni: undefined,
+            empresa: undefined,
+            telefono: undefined,
+            email: undefined
+        }
+    },
+    mounted() {
+    },
+    methods: {
+        onClickSiguiente() {
+            this.nombre = document.getElementById("nombreText").value
+            this.apellidos = document.getElementById("apellidosText").value
+            this.username = document.getElementById("usernameText").value
+            this.contraseña = document.getElementById("contraseñaText").value
+            this.dni = document.getElementById("dniText").value
+            this.empresa = document.getElementById("empresaText").value
+            this.telefono = document.getElementById("telefonoText").value
+            this.telefono = document.getElementById("emailText").value
+        }
+    }
+}
+</script>
 
 <style>
     #groupForm {
--- blablatruck-page/src/views/RegisterClient2.vue
+++ blablatruck-page/src/views/RegisterClient2.vue
@@ -6,20 +6,82 @@
         </b-row>
 
         <h3 id="subtitle" class="mt-3">Datos bancarios</h3>
-
         <label id="labels" class="mt-3 mb-0"> IBAN </label>
-        <b-form-input type="text"> </b-form-input>
+        <b-form-input id="ibanText" type="text"> </b-form-input>
         <label id="labels" class="mt-3 mb-0"> Nº de cuenta bancaria </label>
-        <b-form-input type="text"> </b-form-input>
+        <b-form-input id="ncCuentaBancariaText" type="text"> </b-form-input>
 
         <label id="errorText" class="mt-4 mb-0"> Error </label>
 
         <router-link to="/registerClient1">
             <button id="buttons" class="btn btn-primary mb-2">Atrás</button>
         </router-link>
-        <button id="buttons" class="btn btn-primary mb-2">Crear</button>
+        <button id="buttons" @click="onClickCrear" class="btn btn-primary mb-2">Crear</button>
     </div>
 </template>
+
+<script>
+const axios = require('axios');
+export default {
+    name: "RegisterClient2",
+    data() {
+        return{
+            nombre: undefined,
+            apellidos: undefined,
+            username: undefined,
+            contraseña: undefined,
+            dni: undefined,
+            empresa: undefined,
+            telefono: undefined
+        }
+    },
+    created() {
+        this.nombre = this.$route.params.nombre
+        this.apellidos = this.$route.params.apellidos
+        this.username = this.$route.params.username
+        this.contraseña = this.$route.params.contraseña
+        this.dni = this.$route.params.dni
+        this.empresa = this.$route.params.empresa
+        this.telefono = this.$route.params.telefono
+    },
+    methods: {
+        //Conectarse a la api para crear un cliente
+        onClickCrear() {
+            /*axios.post('localhost:3300/api/personas', {
+                Nombre: this.nombre,
+                Apellidos: this.apellidos,
+                DNI: this.dni,
+                Telefono: this.telefono,
+                Email: this.email,
+                Usuario: this.username,
+                Contraseña: this.contraseña,
+                IBAN: document.getElementById("ibanText").value,
+                NumeroCuentaBancaria: document.getElementById("ncCuentaBancariaText").value
+            }).then((response) => {
+                console.log(response);
+            }, (error) => {
+                console.log(error);
+            });*//*
+            axios.post('localhost:3300/api/personas', {
+                Nombre: 'Jesus',
+                Apellidos: 'Garcia',
+                DNI: '12345687Z',
+                Telefono: '620111111',
+                Email: 'correo@correo.com',
+                Usuario: 'user',
+                Contraseña: 'user',
+                IBAN: 'ES11',
+                NumeroCuentaBancaria: '618253694888314000'
+            }).then((response) => {
+                console.log(response);
+            }, (error) => {
+                console.log(error);
+            });*/
+            axios.get('localhost:3300/api/personas')
+        }
+    }
+}
+</script>
 
 <style>
     #groupForm {
@@ -32,7 +94,8 @@
     }
     #labels{
         text-align: left;
-        width: 500px;
+        max-width: 500px;
+        fill: auto;
     }
     #photo
     {
@@ -42,7 +105,8 @@
     #errorText
     {
         text-align: left;
-        width: 500px;
+        max-width: 500px;
+        fill: auto;
         color: red;
     }
     #title
