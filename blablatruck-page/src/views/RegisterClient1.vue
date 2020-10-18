f591e7ab7d142d397fb8d61608d7c525b586daf4
Alejandro <alcarsa5@inf.upv.es>
2020-10-18 16:26:15 +0200

    Validación campos registerClient.

--- blablatruck-page/src/views/RegisterClient1.vue
+++ blablatruck-page/src/views/RegisterClient1.vue
@@ -8,38 +8,81 @@
         <h3 id="subtitle" class="mt-3">Información personal</h3>
 
         <label id="labels" class="mt-3 mb-0"> Nombre </label>
-        <b-form-input type="text"> </b-form-input>
+        <b-form-input id="nameText" type="text" :state="nameError"> </b-form-input>
         <label id="labels" class="mt-3 mb-0"> Apellidos </label>
-        <b-form-input type="text"> </b-form-input>
+        <b-form-input id="surnamesText" type="text" :state="surnamesError"> </b-form-input>
         <label id="labels" class="mt-3 mb-0"> Nombre de Usuario </label>
-        <b-form-input type="text"> </b-form-input>
+        <b-form-input id="usernameText" type="text" :state="usernameError"> </b-form-input>
         <label id="labels" class="mt-3 mb-0"> Contraseña </label>
-        <b-form-input type="password"> </b-form-input>
+        <b-form-input id="passwordText" type="password" :state="passwordError"> </b-form-input>
         <label id="labels" class="mt-3 mb-0"> DNI </label>
-        <b-form-input type="text"> </b-form-input>
+        <b-form-input id="dniText" type="text" :state="dniError"> </b-form-input>
         <label id="labels" class="mt-3 mb-0"> Empresa </label>
-        <b-form-input type="text"> </b-form-input>
+        <b-form-input id="companyText" type="text" :state="companyError"> </b-form-input>
         <label id="labels" class="mt-3 mb-0"> Teléfono </label>
-        <b-form-input type="tel"> </b-form-input>
-
-        <label id="errorText" class="mt-4 mb-0"> Error </label>
+        <b-form-input id="telephoneText" type="number" :state="telephoneError"> </b-form-input>
 
         <router-link to="/login">
             <button id="buttons" class="btn btn-primary mb-2">Cancelar</button>
         </router-link>
         <router-link to="/registerClient2">
-            <button id="buttons" class="btn btn-primary mb-2">Siguiente</button>
+            <button id="buttons" @click="next" class="btn btn-primary mb-2">Siguiente</button>
         </router-link>
     </div>
 </template>
+
+<script>
+/* eslint-disable */
+export default {
+    name: "RegisterClient1",
+    
+    data() {
+        return {
+            nameError: undefined,
+            surnamesError: undefined,
+            usernameError: undefined,
+            passwordError: undefined,
+            dniError: undefined,
+            companyError: undefined,
+            telephoneError: undefined,
+        };
+    },
+    methods: {
+        next () {
+            // Campo para saber si hay error
+            var error = false
+
+            //Recoger los datos de los campos
+            var nameText = document.getElementById("nameText")
+            var surnamesText = document.getElementById("surnamesText")
+            var usernameText = document.getElementById("usernameText")
+            var passwordText = document.getElementById("passwordText")
+            var dniText = document.getElementById("dniText")
+            var companyText = document.getElementById("companyText")
+            var telephoneText = document.getElementById("telephoneText")
+
+            //Comprobación de campos
+            if (nameText.value == "") { this.nameError = false; error = true}
+            if (surnamesText.value == "") { this.surnamesError = false; error = true}
+            if (usernameText.value == "") { this.usernameError = false; error = true}
+            if (passwordText.value == "") { this.passwordError = false; error = true}
+            if (dniText.value == "") { this.dniError = false; error = true}
+            if (companyText.value == "") { this.companyError = false; error = true}
+            if (telephoneText.value == "") { this.telephoneError = false; error = true}
+
+            if (!error) ;
+        }
+    }
+
+}
+</script>
 
 <style>
     #groupForm {
         max-width: 500px;
     }
     #buttons{
-        margin: 10%;        
-        margin-top: 5%;
+        margin: 10%;
         width: 100px;
     }
     #labels{
@@ -50,12 +93,6 @@
     {
         height: 100px;
         width: 100px;
-    }
-    #errorText
-    {
-        text-align: left;
-        width: 500px;
-        color: red;
     }
     #title
     {
--- blablatruck-page/src/views/RegisterClient2.vue
+++ blablatruck-page/src/views/RegisterClient2.vue
@@ -8,18 +8,47 @@
         <h3 id="subtitle" class="mt-3">Datos bancarios</h3>
 
         <label id="labels" class="mt-3 mb-0"> IBAN </label>
-        <b-form-input type="text"> </b-form-input>
+        <b-form-input id="ibanText" type="text" :state="ibanError"> </b-form-input>
         <label id="labels" class="mt-3 mb-0"> Nº de cuenta bancaria </label>
-        <b-form-input type="text"> </b-form-input>
-
-        <label id="errorText" class="mt-4 mb-0"> Error </label>
+        <b-form-input id="bankAccountText" type="number" :state="bankAccountError"> </b-form-input>
 
         <router-link to="/registerClient1">
             <button id="buttons" class="btn btn-primary mb-2">Atrás</button>
         </router-link>
-        <button id="buttons" class="btn btn-primary mb-2">Crear</button>
+        <button id="buttons" @click="createClient" class="btn btn-primary mb-2">Crear</button>
     </div>
 </template>
+
+<script>
+/* eslint-disable */
+export default {
+    name: "RegisterClient1",
+    
+    data() {
+        return {
+            ibanError: undefined,
+            bankAccountError: undefined,
+        };
+    },
+    methods: {
+        createClient () {
+            // Campo para saber si hay error
+            var error = false
+
+            //Recoger los datos de los campos
+            var ibanText = document.getElementById("ibanText")
+            var bankAccountText = document.getElementById("bankAccountText")
+
+            //Comprobación de campos
+            if (ibanText.value == "") { this.ibanError = false; error = true}
+            if (bankAccountText.value == "") { this.bankAccountError = false; error = true}
+
+            if (!error) ;
+        }
+    }
+
+}
+</script>
 
 <style>
     #groupForm {
@@ -27,7 +56,6 @@
     }
     #buttons{
         margin: 10%;
-        margin-top: 5%;
         width: 100px;
     }
     #labels{
@@ -38,12 +66,6 @@
     {
         height: 100px;
         width: 100px;
-    }
-    #errorText
-    {
-        text-align: left;
-        width: 500px;
-        color: red;
     }
     #title
     {
