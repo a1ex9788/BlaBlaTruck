94fed0606007e5d1733b3415c2a759fd6dc85fe4
pabvazga <pabvazga@inf.upv.es>
2020-10-20 16:35:42 +0200

    trabajo en crear encargo

--- blablatruck-page/src/views/RegisterNewPackage.vue
+++ blablatruck-page/src/views/RegisterNewPackage.vue
@@ -1,12 +1,7 @@
 <template>
+
 <div id="groupForm" class="container-fluid">
-
-    <div hidden id="showPriceCalculated">
-        <h1> Precio </h1>
-        <b-button class="mt-2 mx-4" type="submit" variant="primary">Crear encargo</b-button>
-        <b-button class="mt-2" type="button">Volver</b-button>
-    </div>
-
+    
     <div id="priceCalculation">
 
         <h1 class="mt-0">Calcula el presupuesto de tu pedido</h1>
@@ -14,25 +9,25 @@
         <b-form @submit="onSubmit" @reset="onReset" v-if="show">
 
             <b-form-group id="input-group-1" label="Origen:" label-for="input-1">
-                <b-form-input id="input-Origin" type="text" required placeholder="Direccion: Calle | nrº | planta | puerta"></b-form-input>
+                <b-form-input id="input-Origin" v-model= form.origin type="text" required placeholder="Direccion: Calle | nrº | planta | puerta"> </b-form-input>
             </b-form-group>
 
             <b-form-group id="input-group-2" label="Destino:" label-for="input-2">
-                <b-form-input id="input-Destination" type="text" required placeholder="Direccion: Calle | nrº | planta | puerta"></b-form-input>
+                <b-form-input id="input-Destination"  v-model= form.destination type="text" required placeholder="Direccion: Calle | nrº | planta | puerta"></b-form-input>
             </b-form-group>
 
-            <b-form-group id="input-group-3" label="Peso mercancia (en kg):" label-for="input-3">
+            <b-form-group id="input-group-3" label="Peso mercancia:" label-for="input-3">
                 <b-input-group class="mt-3 ">
                     <template v-slot:append>
                         <b-input-group-text><strong class="text">kg</strong></b-input-group-text>
                     </template>
-                    <b-form-input id="input-Weight" max="32" min="0" value="1" type="number"></b-form-input>
+                    <b-form-input id="input-Weight" v-model="form.weight" max="32" min="0" value="1" type="number"></b-form-input>
                 </b-input-group>
 
             </b-form-group>
 
             <b-form-group id="input-group-Nature" label="Naturaleza:" label-for="input-Nature">
-                <b-form-select id="input-Nature" v-model="form.nature" :options="nature" required></b-form-select>
+                <b-form-select id="input-Nature" v-model="form.nature" :options="optionsNature" required></b-form-select>
             </b-form-group>
 
             <label>Tamaño</label>
@@ -43,7 +38,7 @@
                     <template v-slot:append>
                         <b-input-group-text><strong class="text">m</strong></b-input-group-text>
                     </template>
-                    <b-form-input placeholder="Ancho"></b-form-input>
+                    <b-form-input v-model="form.size[0]"  placeholder="Ancho"></b-form-input>
                 </b-input-group>
 
                 <label class="mt-2 mx-3">x</label>
@@ -52,7 +47,7 @@
                     <template v-slot:append>
                         <b-input-group-text><strong class="text">m</strong></b-input-group-text>
                     </template>
-                    <b-form-input placeholder="Alto"></b-form-input>
+                    <b-form-input v-model="form.size[1]" placeholder="Alto"></b-form-input>
                 </b-input-group>
 
                 <label class="mt-2 mx-3">x</label>
@@ -61,17 +56,23 @@
                     <template v-slot:append>
                         <b-input-group-text><strong class="text">m</strong></b-input-group-text>
                     </template>
-                    <b-form-input placeholder="Largo"></b-form-input>
+                    <b-form-input v-model="form.size[2]" placeholder="Largo"></b-form-input>
                 </b-input-group>
 
             </b-form>
 
             <br>
             <b-button @click="showPrice" class="mt-2 mx-4" type="submit" variant="primary">Calcular</b-button>
-            <b-button class="mt-2" type="button">Volver</b-button>
+            <b-button @click='goBackPage' class="mt-2" type="button">Volver</b-button>
         </b-form>
-
     </div>
+
+    <div id="showPriceCalculated" v-if="!show">
+        <h2> [Precio calculado]</h2>
+        <b-button class="mt-2 mx-4" type="submit" variant="primary">Crear encargo</b-button>
+        <b-button @click="goBack" class="mt-2" type="button">Volver</b-button>
+    </div>
+
 </div>
 </template>
 
@@ -83,12 +84,18 @@ export default {
             form: {
                 origin: '',
                 destination: '',
-                weight: ''
+                weight: '0',
+                nature: 'null',
+                size: [],
             },
-            nature: [{
-                text: 'Elige una opcion..',
-                value: null
-            }, 'Normal', 'Fragil', 'Congelado'],
+
+           optionsNature: [
+          { value: 'null', text: 'Selecciona una opción' },
+          { value: 'Frágil', text: 'Frágil' },
+          { value:  'Congelado', text: 'Congelado' },
+          { value: 'Normal', text: 'Normal' },
+            ],
+
             show: true
         }
     },
@@ -99,9 +106,20 @@ export default {
         },
 
         showPrice() {
-            document.getElementById("priceCalculation").hidden = true
-            document.getElementById("showPriceCalculated").hidden = false
+            //document.getElementById("priceCalculation").hidden = true
+            //document.getElementById("showPriceCalculated").hidden = false
+            this.show = false
+       },
+
+        goBackPage(){
+            this.$router.push('about'); //Falta indicar la verdadera página de retorno
         },
+
+        goBack(){
+            this.show=true;
+
+        },
+
         onReset(evt) {
             evt.preventDefault()
             // Reset our form values
