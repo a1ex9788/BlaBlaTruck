ccbc113ded4092fbc7ea56efb7a0d4b0df3b84e6
pabvazga <pabvazga@inf.upv.es>
2020-10-20 20:27:26 +0200

    validacion de campos

--- blablatruck-page/src/views/RegisterNewPackage.vue
+++ blablatruck-page/src/views/RegisterNewPackage.vue
@@ -4,16 +4,29 @@
     
     <div id="priceCalculation">
 
-        <h1 class="mt-0">Calcula el presupuesto de tu pedido</h1>
+        <h1 class="mt-0">Calcula el presupuesto de tu pedido :)</h1>
 
         <b-form @submit="onSubmit" @reset="onReset" v-if="show">
 
-            <b-form-group id="input-group-1" label="Origen:" label-for="input-1">
-                <b-form-input id="input-Origin" v-model= form.origin type="text" required placeholder="Direccion: Calle | nrº | planta | puerta"> </b-form-input>
+         <b-form-group id="input-group-1" label="Origen:" label-for="input-1">
+             <b-form-input 
+             id="input-Origin" 
+              v-model="$v.form.origin.$model"
+              :state="validateState('origin')"
+              type="text"
+              placeholder="Direccion: Calle | nrº | planta | puerta"> 
+              </b-form-input>
             </b-form-group>
 
-            <b-form-group id="input-group-2" label="Destino:" label-for="input-2">
-                <b-form-input id="input-Destination"  v-model= form.destination type="text" required placeholder="Direccion: Calle | nrº | planta | puerta"></b-form-input>
+             <b-form-group id="input-group-2" label="Destino:" label-for="input-2">
+             <b-form-input 
+             id="input-Destination" 
+              v-model="$v.form.destination.$model"
+              :state="validateState('destination')"
+              type="text"
+              placeholder="Direccion: Calle | nrº | planta | puerta"> 
+              </b-form-input>
+
             </b-form-group>
 
             <b-form-group id="input-group-3" label="Peso mercancia:" label-for="input-3">
@@ -27,7 +40,7 @@
             </b-form-group>
 
             <b-form-group id="input-group-Nature" label="Naturaleza:" label-for="input-Nature">
-                <b-form-select id="input-Nature" v-model="form.nature" :options="optionsNature" required></b-form-select>
+                <b-form-select id="input-Nature"  v-model="$v.form.nature.$model" :options="optionsNature" :state="validateState('nature')"></b-form-select>
             </b-form-group>
 
             <label>Tamaño</label>
@@ -38,7 +51,10 @@
                     <template v-slot:append>
                         <b-input-group-text><strong class="text">m</strong></b-input-group-text>
                     </template>
-                    <b-form-input v-model="form.size[0]"  placeholder="Ancho"></b-form-input>
+                    <b-form-input 
+                    v-model= "$v.form.size.ancho.$model"
+                    :state= "validateSize('ancho')"
+                    placeholder="Ancho"></b-form-input>
                 </b-input-group>
 
                 <label class="mt-2 mx-3">x</label>
@@ -47,7 +63,10 @@
                     <template v-slot:append>
                         <b-input-group-text><strong class="text">m</strong></b-input-group-text>
                     </template>
-                    <b-form-input v-model="form.size[1]" placeholder="Alto"></b-form-input>
+                    <b-form-input 
+                     v-model= "$v.form.size.alto.$model" 
+                     :state= "validateSize('alto')"
+                     placeholder="Alto"></b-form-input>
                 </b-input-group>
 
                 <label class="mt-2 mx-3">x</label>
@@ -56,7 +75,11 @@
                     <template v-slot:append>
                         <b-input-group-text><strong class="text">m</strong></b-input-group-text>
                     </template>
-                    <b-form-input v-model="form.size[2]" placeholder="Largo"></b-form-input>
+                    <b-form-input 
+                    v-model= "$v.form.size.largo.$model" 
+                    :state= "validateSize('largo')"
+                    placeholder="Largo">
+                    </b-form-input>
                 </b-input-group>
 
             </b-form>
@@ -77,68 +100,107 @@
 </template>
 
 <script>
+import { validationMixin } from "vuelidate";
+import {required} from "vuelidate/lib/validators";
+
 export default {
-    
-    data() {
-        return {
-            form: {
-                origin: '',
-                destination: '',
-                weight: '0',
-                nature: 'null',
-                size: [],
-            },
+  mixins: [validationMixin],
+  name: "FormComponent",
+  data() {
+    return {
+      form: {
+        name: null,
+        origin: "",
+        destination: "",
+        weight: "0",
+        nature: null,
+        size: {
+          ancho: "",
+          largo: "",
+          alto: "",
+        },
+      },
 
-           optionsNature: [
-          { value: 'null', text: 'Selecciona una opción' },
-          { value: 'Frágil', text: 'Frágil' },
-          { value:  'Congelado', text: 'Congelado' },
-          { value: 'Normal', text: 'Normal' },
-            ],
+      optionsNature: [
+        { value: null, text: "Selecciona una opción" },
+        { value: "Frágil", text: "Frágil" },
+        { value: "Congelado", text: "Congelado" },
+        { value: "Normal", text: "Normal" },
+      ],
 
-            show: true
-        }
+      show: true,
+    };
+  },
+
+  validations: {
+    form: {
+      origin: {
+        required,
+      },
+
+      destination: {
+        required,
+      },
+
+      nature: {
+        required,
+      },
+
+      size: {
+        ancho: { required },
+        largo: { required },
+        alto: { required },
+      },
     },
-    methods: {
-        onSubmit(evt) {
-            evt.preventDefault()
-            alert(JSON.stringify(this.form))
-        },
+  },
 
-        showPrice() {
-            //document.getElementById("priceCalculation").hidden = true
-            //document.getElementById("showPriceCalculated").hidden = false
-            this.show = false
-       },
+  methods: {
+    validateState(prop) {
+      const { $dirty, $error } = this.$v.form[prop];
+      return $dirty ? !$error : null;
+    },
 
-        goBackPage(){
-            this.$router.push('about'); //Falta indicar la verdadera página de retorno
-        },
+    validateSize(prop) {
+      const { $dirty, $error } = this.$v.form.size[prop];
+      return $dirty ? !$error : null;
+    },
 
-        goBack(){
-            this.show=true;
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
 
-        },
+    showPrice() {
+      this.show = false;
+    },
 
-        onReset(evt) {
-            evt.preventDefault()
-            // Reset our form values
-            this.form.origin = ''
-            this.form.destination = ''
-            this.form.nature = null
-            // Trick to reset/clear native browser form validation state
-            this.show = false
-            this.$nextTick(() => {
-                this.show = true
-            })
-        }
-    }
-}
+    goBackPage() {
+      this.$router.push("about"); //Falta indicar la verdadera página de retorno
+    },
+
+    goBack() {
+      this.show = true;
+    },
+
+    onReset(evt) {
+      evt.preventDefault();
+      // Reset our form values
+      this.form.origin = "";
+      this.form.destination = "";
+      this.form.nature = null;
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+  },
+};
 </script>
 
 <style>
 #groupForm {
-    max-width: 500px;
-    margin: 0 auto;
+  max-width: 500px;
+  margin: 0 auto;
 }
 </style>
